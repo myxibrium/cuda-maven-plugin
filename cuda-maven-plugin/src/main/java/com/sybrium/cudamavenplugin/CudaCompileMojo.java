8e9fb2375b4272eccc64084a141651c432bd8fda
james <james.watkins.2009@gmail.com>
2015-02-08 01:31:57 -0500

    Initial Commit

---
+++ cuda-maven-plugin/src/main/java/com/sybrium/cudamavenplugin/CudaCompileMojo.java
@@ -0,0 +1,119 @@
+package com.sybrium.cudamavenplugin;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+/**
+ * Compiles CUDA source files using nvcc, then puts the resulting *.ptx files on
+ * the classpath.
+ */
+@Mojo(name = "compile-cuda")
+public class CudaCompileMojo extends AbstractMojo {
+
+    /**
+     * The source folder where CUDA source files are located.
+     */
+    @Parameter(property = "baseDirectory", defaultValue = "${project.basedir}/src/main/cuda")
+    private File baseDirectory;
+
+    /**
+     * The extensions of the files to search for. Should leave this default.
+     */
+    @Parameter(property = "extensions", defaultValue = "cu")
+    private String[] extensions;
+
+    /**
+     * The directory where the resulting ptx files go.
+     */
+    @Parameter(property = "outputDirectory", defaultValue = "${project.build.outputDirectory}")
+    private File outputDirectory;
+
+    /**
+     * Determines whether the path of the source file relative to the
+     * baseDirectory should be preserved in the ooutputDirectory. Should leave
+     * this default.
+     */
+    @Parameter(property = "preservePath", defaultValue = "true")
+    private Boolean preservePath;
+
+    public void execute() throws MojoExecutionException {
+        Collection<File> inputFiles = FileUtils.listFiles(baseDirectory, extensions, true);
+
+        getLog().info("Compiling " + inputFiles.size() + " cuda source file(s) to " + outputDirectory + ",");
+
+        for (File inputFile : inputFiles) {
+            String relativePath = getRelativePath(inputFile);
+            String outputFileName;
+            File outputFile;
+            try {
+                if (preservePath) {
+                    outputFileName = stripExtension(outputDirectory.getCanonicalPath() + relativePath) + ".ptx";
+                } else {
+                    outputFileName = stripExtension(outputDirectory.getCanonicalPath() + File.separator
+                            + inputFile.getName())
+                            + ".ptx";
+                }
+                outputFile = new File(outputFileName);
+                outputFile.getParentFile().mkdirs();
+            } catch (IOException e) {
+                throw new MojoExecutionException("Could not create output directories.", e);
+            }
+            try {
+                String command = "nvcc -ptx " + inputFile.getCanonicalPath() + " -o " + outputFileName;
+                Process process = Runtime.getRuntime().exec(command);
+                int exitValue = 0;
+                try {
+                    exitValue = process.waitFor();
+                } catch (InterruptedException e) {
+                    throw new MojoExecutionException("The nvcc command was interrupted.", e);
+                }
+                if (exitValue != 0) {
+                    throw new MojoExecutionException("The nvcc command return non-zero, meaning it failed.");
+                }
+                getLog().info("Compiled " + outputFileName);
+            } catch (IOException e) {
+                throw new MojoExecutionException("Could not compile", e);
+            }
+        }
+    }
+
+    private String getRelativePath(File inputFile) throws MojoExecutionException {
+        try {
+            return inputFile.getCanonicalPath().substring(baseDirectory.getCanonicalPath().length());
+        } catch (IOException e) {
+            throw new MojoExecutionException("Could not get relative path of resource.", e);
+        }
+    }
+
+    private String stripExtension(String str) {
+        int lastIndexOfPeriod = str.lastIndexOf('.');
+        if (lastIndexOfPeriod == -1) {
+            return str;
+        } else {
+            return str.substring(0, lastIndexOfPeriod);
+        }
+    }
+}
